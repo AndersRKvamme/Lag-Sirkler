a6d205aaeaa975d4752c1416249d4bf17fd9ff37
AndersRKvamme <rka092024@gmail.com>
2024-12-03 11:05:14 +0100

    Lets a g

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ const makeCircles = () => {
   window.alert("Please write a number between 0 and 1001.");
 }    
     else{
+        const circles = document.querySelectorAll (".circle");
+        for (circle of circles) {
+            circle.remove();
+        }
         //make circles
         for (let i = 0; i < inputNum.value; i++) {
-            const circle = document.createElement("div")
-            circle.classList.add("circle")
+            const circle = document.createElement("div");
+            circle.classList.add("circle");
+            circle.textContent = i+1;
+            circle.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            circleContainer.append(circle);
         }
-        
-
    }
 };
